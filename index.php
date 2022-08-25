7b29cdccf88d6aff398fcd2169073ac5ef974d93
david.propitech <david.propitech@gmail.com>
2022-08-25 16:10:14 +0300

    davidcv

---
+++ index.php
@@ -0,0 +1,104 @@
+<div class="parent">
+    <div class="div2"> <img src="zaodavid.png" alt="" srcset=""></div>
+    <div class="div3">
+        <h1>Davidson ANDRIAMASINAVALONA</h1>
+        <p>Développeur</p>
+        <p>andrisondavidmail@gmail.com</p>
+        <p>034 60 131 54</p>
+        <p>033 90 656 71</p>
+        <p>Lot IAK 181 Itaosy Marobiby, 102 Antananarivo</p>
+    </div>
+</div>
+<div class="parent2">
+    <div class="div12">
+        <h2>Expérience professionnelle</h2>
+        <div class="section">
+            <p><b>Développeur PHP</b></p>
+            <p>CENTIC-MADA, Ambanidia Antananarivo</p>
+            <ul>
+                <li>Symfony</li>
+                <li>Codeigniter</li>
+            </ul>
+            <p><b>Développeur WordPresse</b></p>
+            <p>MIANJAIKA Communication , Antananarivo</p>
+            <p><b>Développeur PHP</b></p>
+            <p>PROPITECH, Antananarivo</p> 
+        </div>
+        <h2>Diplômes</h2>
+
+        <div class="section">
+            <p>Licence en Informatique- Base de données et Génie Logiciel
+            CNTEMAD,  Antananarivo</p>
+        </div>
+           
+        <h2>Compétences</h2>
+        <div class="section">
+            <ul>
+            <li>Programmation Informatique</li>
+            <li>Programmation Orienter Objet</li>
+            <li>Conception Base de Données</li>
+            <li>Architecture MVC (Modèles Vues Contrôleurs)</li>
+            <li>Maintenance et création site web</li>
+            <li>Système de contrôle de version Git</li>
+            <li>Maîtrise de l'environnement Linux, Shell</li>
+            <li>Création plugin wordpress</li>
+            <li>Création plugin SPIP</li>
+            <li>Web service: REST</li>
+            <li>Web scraping</li>
+            </ul>
+        </div>
+
+        <h2>Connaissance informatique</h2>
+        <div class="section">
+            <p><b>LANGAGE</b> : PHP, HTML, CSS, SQL,  JAVASCRIPT</p>
+            <p><b>BASE DE DONNÉES</b>: MySQL</p>
+            <p><b>FRAMEWORK</b>: Bootstrap, Codeigniter,  Jquery, Symfony3</p>
+            <p><b>CMS</b>: WordPress, spip</p>
+        </div>
+        
+    </div>
+    <div class="div22">
+        <h2>Informations personnelles</h2>
+        <p>Date de naissance : 18 janvier 1995</p>
+        <p>Permis de conduire: A</p>
+        <p>Nationalité: Malagasy</p>
+        <p>Etat civil: Marié, 2 fils</p>
+    </div>
+</div>
+
+<style>
+    .parent2 {
+        display: grid;
+        grid-template-columns: 4fr 1fr;
+        grid-template-rows: 1fr;
+        grid-column-gap: 0px;
+        grid-row-gap: 0px;
+        }
+
+.div12 { grid-area: 1 / 1 / 2 / 2; }
+.div22 { grid-area: 1 / 2 / 2 / 3; }
+
+    .parent {
+    display: grid;
+    grid-template-columns: 1fr 4fr;
+    grid-template-rows: 1fr;
+    grid-column-gap: 0px;
+    grid-row-gap: 0px;
+    }
+
+.div2 { grid-area: 1 / 1 / 2 / 2; }
+.div3 { grid-area: 1 / 2 / 2 / 3; 
+    background-color: #849ab7;
+    padding: 6px;
+    color: white;
+    }
+    body {
+    font-family: sans-serif;
+}
+h2 {
+    color: #849ab7;
+}
+.section {
+    padding: 10px;
+}
+</style>
